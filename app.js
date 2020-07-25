5d08afe32be3d492559d5c293d5ccfc466159acd
Harsh1425 <68772573+Harsh1425@users.noreply.github.com>
2020-07-25 14:48:40 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,17 +3,16 @@
 
 ///Edit Under This
 
-var firebaseConfig = {
-  apiKey: "api-key",
-  authDomain: "project-id.firebaseapp.com",
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: "project-id",
-  storageBucket: "project-id.appspot.com",
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id",
+const firebaseConfig = {
+  apiKey: "AIzaSyBJVflmh6Okqc-qOrox5WwUZOwuMqo1sIM",
+  authDomain: "instagrom-e1710.firebaseapp.com",
+  databaseURL: "https://instagrom-e1710.firebaseio.com",
+  projectId: "instagrom-e1710",
+  storageBucket: "instagrom-e1710.appspot.com",
+  messagingSenderId: "969188051474",
+  appId: "1:969188051474:web:3b1b08df37be6437a226cb",
+  measurementId: "G-JXVLLP09JY"
 };
-
      
  
 ////Do Not Edit Below This
